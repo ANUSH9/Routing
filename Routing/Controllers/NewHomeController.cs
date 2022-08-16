a492f41386aac879be66557b3d2cc357fbe3bd5a
ANUSH9 <loginanushgupta@gmail.com>
2022-08-16 14:58:45 +0530

    Add project files.

---
+++ Routing/Controllers/NewHomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Routing.Controllers
+{
+    public class NewHomeController : Controller
+    {
+        [Route("[controller]/[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("")]
+        [Route("Product")]
+        [Route("Product/ProductList")]
+        [Route("Product/ProductList/{id:int}/{str}")]
+        public IActionResult ProductById(int id, string str)
+        {
+            return Content($"Blog entry with ID #{id} requested (URL Slug: {str})");
+            // return View();
+        }
+    }
+}
